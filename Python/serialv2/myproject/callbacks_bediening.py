f8ca74c5c45832042c4301d60187f5e56d1e3d75
DLatumalea <d.latumalea@hotmail.com>
2018-11-14 15:30:01 +0100

    Add files via upload

--- Python/serialv2/myproject/callbacks_bediening.py
+++ Python/serialv2/myproject/callbacks_bediening.py
@@ -1,4 +1,4 @@
-from .server import app, ar
+from .server import app, ar, autonoom
 from dash.dependencies import Input, Output
 import dash_html_components as html
 import dash_core_components as dcc
@@ -51,9 +51,22 @@ def show_date(n):
 
 # return output range slider
 #if ar.get_state() == True:
+#@app.callback(
+#    Output('output-range-slider', 'children'),
+#    [Input('uitrol_bereik_handmatig', 'value')]
+#)
+#def get_output(value):
+#    value_min = value[0]
+#    value_max = value[1]
+#    ar.write('exth {}'.format(value_min))
+#    ar.readline()
+#    ar.write('extl {}'.format(value_max))
+#    return "Min: {}, Max: {}".format(value_min, value_max)
+    #returns a list
+
 @app.callback(
-    Output('output-range-slider', 'children'),
-    [Input('uitrol_bereik_handmatig', 'value')]
+    Output('temperatuur_bereik_autonoom_P', 'children'),
+    [Input('temperatuur_bereik_autonoom', 'value')]
 )
 def get_output(value):
     value_min = value[0]
@@ -61,5 +74,25 @@ def get_output(value):
     ar.write('tmpl {}'.format(value_min))
     ar.readline()
     ar.write('tmph {}'.format(value_max))
+    ar.readline()
     return "Min: {}, Max: {}".format(value_min, value_max)
-    #returns a list
+
+
+@app.callback(
+    Output('uitrol_bereik_autonoom_P', 'children'),
+    [Input('uitrol_bereik_autonoom', 'value')]
+)
+def get_output(value):
+    value_min = value[0]
+    value_max = value[1]
+    ar.write('extl {}'.format(value_min))
+    ar.readline()
+    ar.write('exth {}'.format(value_max))
+    ar.readline()
+    return "Min: {}, Max: {}".format(value_min, value_max)
+
+
+'''
+output was html.Span
+input was temperatuur_bereik_autonoom
+'''
--- Python/serialv2/myproject/server.py
+++ Python/serialv2/myproject/server.py
@@ -15,5 +15,5 @@ app.config['suppress_callback_exceptions']=True
 com = Arduino.get_com()
 ar = Arduino(com, 19200)
 ar.connect()
-ar.get_data()
-ar.get_settings()
+
+autonoom = False
